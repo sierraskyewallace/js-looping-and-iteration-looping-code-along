a748977267081636826c2a937b678ac2325c5d94
sierraskyewallace <sskwallace@gmail.com>
2021-07-23 10:48:59 -0700

    done

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+
+
+
+function writeCards(names, eventName){
+    let cards = [];
+    for (let i = 0; i < names.length; i++) {
+        cards[i] = `Thank you, ${names[i]}, for the wonderful ${eventName} gift!`;
+    }
+    return cards;
+}
+
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number);
+        number -= 1;
+    }
+} 
